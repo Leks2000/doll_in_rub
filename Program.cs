dda8327c1c22c8dcc1ef5ac9fe381b2b3ff00a31
ддд <@DESKTOP-OMI23JR>
2022-09-07 00:43:11 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// ввести количество денег 1 валюты, вывести курс валюты 1 - 2, процент налога, если < 500 - 8, > 500 - %
+{
+    string str;
+    Console.Write("Введите количество денег: ");
+    str = Console.ReadLine();
+    int a = int.Parse(str);
+    int b = 65;
+    float c = 0.37f;
+    int result = a * b;
+    int result1 = result - 8;
+    float result2 = result - (result * c / 100);
+     Console.WriteLine("1 доллар = " + b);
+        Console.WriteLine("Процент налога: " + c);
+     Console.WriteLine("С перевода на нынешний курс получилось без налога: " + result);
+    if (a < 500)
+    {
+        Console.WriteLine("С перевода на нынешний курс получилось с налогом: " + result1);
+    }
+    else
+    {
+        Console.WriteLine("С перевода на нынешний курс получилось с налогом: " + result2);
+    }
+}
